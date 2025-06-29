fba56417c423ec9524cfa6f2e19b4a0fcc78aa10
Khalipha-Saliso <129382642+Khalipha-Saliso@users.noreply.github.com>
2025-06-29 10:59:20 +0200

    add basic from handling did typewriter plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,12 @@
+function generatePoem(event){
+    event.preventDefault()
 
+    new Typewriter('#poem-output', {
+      strings: ['Poem Lines', 'Poem Lines'],
+      autoStart: true,
+    });
+    poemElement.innerHTML = "Poem Lines";
+}
+
+let poemFromElement = document.querySelector('#poem-form');
+poemFromElement.addEventListener("submit", generatePoem);
